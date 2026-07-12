b3c6638478fa9a1ae9e8f6347f0b212f6f3fc039
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "spring_cloud_certificates" {
-  description = "All spring_cloud_certificate resources"
-  value       = azurerm_spring_cloud_certificate.spring_cloud_certificates
-}
 output "spring_cloud_certificates_certificate_content" {
-  description = "List of certificate_content values across all spring_cloud_certificates"
-  value       = [for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : v.certificate_content]
+  description = "Map of certificate_content values across all spring_cloud_certificates, keyed the same as var.spring_cloud_certificates"
+  value       = { for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : k => v.certificate_content }
 }
 output "spring_cloud_certificates_exclude_private_key" {
-  description = "List of exclude_private_key values across all spring_cloud_certificates"
-  value       = [for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : v.exclude_private_key]
+  description = "Map of exclude_private_key values across all spring_cloud_certificates, keyed the same as var.spring_cloud_certificates"
+  value       = { for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : k => v.exclude_private_key }
 }
 output "spring_cloud_certificates_key_vault_certificate_id" {
-  description = "List of key_vault_certificate_id values across all spring_cloud_certificates"
-  value       = [for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : v.key_vault_certificate_id]
+  description = "Map of key_vault_certificate_id values across all spring_cloud_certificates, keyed the same as var.spring_cloud_certificates"
+  value       = { for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : k => v.key_vault_certificate_id }
 }
 output "spring_cloud_certificates_name" {
-  description = "List of name values across all spring_cloud_certificates"
-  value       = [for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : v.name]
+  description = "Map of name values across all spring_cloud_certificates, keyed the same as var.spring_cloud_certificates"
+  value       = { for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : k => v.name }
 }
 output "spring_cloud_certificates_resource_group_name" {
-  description = "List of resource_group_name values across all spring_cloud_certificates"
-  value       = [for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : v.resource_group_name]
+  description = "Map of resource_group_name values across all spring_cloud_certificates, keyed the same as var.spring_cloud_certificates"
+  value       = { for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : k => v.resource_group_name }
 }
 output "spring_cloud_certificates_service_name" {
-  description = "List of service_name values across all spring_cloud_certificates"
-  value       = [for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : v.service_name]
+  description = "Map of service_name values across all spring_cloud_certificates, keyed the same as var.spring_cloud_certificates"
+  value       = { for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : k => v.service_name }
 }
 output "spring_cloud_certificates_thumbprint" {
-  description = "List of thumbprint values across all spring_cloud_certificates"
-  value       = [for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : v.thumbprint]
+  description = "Map of thumbprint values across all spring_cloud_certificates, keyed the same as var.spring_cloud_certificates"
+  value       = { for k, v in azurerm_spring_cloud_certificate.spring_cloud_certificates : k => v.thumbprint }
 }
 
